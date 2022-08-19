24ae45de291860e97cfc859aa6dfd62a2dc9f1a0
Artur Caetano <77642129+arturcaetano98@users.noreply.github.com>
2022-08-19 09:02:53 -0300

    .
    .

---
+++ rgl.r
@@ -0,0 +1,15 @@
+# Usando o "rgl" com R (Não funciona no Colab)
+
+#Instalando o "rgl"
+#install.packages("rgl")
+#install.packages("remotes")
+
+# Link com as explicações
+# https://cran.r-project.org/web/packages/rgl/readme/README.html
+
+# Baixando o projeto do GitHub para testar
+# remotes::install_github("dmurdoch/rgl")
+
+library(rgl)
+browseVignettes("rgl")
+demo(rgl)
